3c246178cd6d6af6242bb2fe8542b37dfb3bdc0d
Hugo Bernardo <hugo.bernardo@pluto.tv>
2024-04-04 11:30:36 -0300

    DB schema (#6)

--- service-gamification/src/database/models/models.go
+++ service-gamification/src/database/models/models.go
@@ -4,5 +4,21 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
 	ID primitive.ObjectID `bson:"_id"`
-	// TODO
+}
+
+type UserEvent struct {
+	ID primitive.ObjectID `bson:"_id"`
+
+	UserID        primitive.ObjectID `bson:"userId"`
+	Type          string             `bson:"type"`
+	TimeWatchedMs int64              `bson:"timeWatchedMs"`
+	Channel       string             `bson:"channel"`
+	Series        string             `bson:"series"`
+	EpisodeNumber int64              `bson:"episodeNumber"`
+	Category      string             `bson:"category"`
+}
+
+type UserPopulated struct {
+	ID     primitive.ObjectID `bson:"_id"`
+	Events []UserEvent
 }
