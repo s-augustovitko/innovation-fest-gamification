8cc396f34c3f11fca1342b184c9ab897fb5af702
Thiago Martins <thiago.martins@pluto.tv>
2024-04-04 15:55:46 -0500

    Removed todos reducer

--- client-gamification/src/app/store.ts
+++ client-gamification/src/app/store.ts
@@ -1,17 +1,15 @@
-import { todoSlice } from "./features/todos.ts";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { api } from "./features/api.ts";
 
 const rootReducer = combineReducers({
-    todos: todoSlice.reducer,
-    [api.reducerPath]: api.reducer,
+    [api.reducerPath]: api.reducer
 });
 
 export const store = configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(api.middleware),
+        getDefaultMiddleware().concat(api.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
