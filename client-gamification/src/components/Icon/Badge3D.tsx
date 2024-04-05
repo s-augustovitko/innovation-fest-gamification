5655c082e450a9a4679eeb82f7706cd9cb44242b
Thiago Martins <thiago.martins@pluto.tv>
2024-04-05 16:59:37 -0500

    patch models

--- client-gamification/src/components/Icon/Badge3D.tsx
+++ client-gamification/src/components/Icon/Badge3D.tsx
@@ -1,41 +1,50 @@
-import {Center, Float, Resize, useGLTF} from '@react-three/drei'
+import { Center, Float, Resize, useGLTF } from "@react-three/drei";
+import { useEffect, useState } from "react";
 
-const MODEL_MAP = {
-    'sandwich': '/models/sandwich/scene.gltf',
-    'tmnt': '/models/tmnt/scene.gltf',
-    'rocko': '/models/rocko/scene.gltf',
-    'invbub': '/models/invbub/scene.gltf',
-    'gary': '/models/gary/scene.gltf',
-    'catdog': '/models/catdog/scene.gltf',
-}
+export const MODEL_MAP = {
+    "sandwich": "/models/sandwich/scene.gltf",
+    "tmnt": "/models/tmnt/scene.gltf",
+    "rocko": "/models/rocko/scene.gltf",
+    "gary": "/models/gary/scene.gltf",
+    "catdog": "/models/catdog/scene.gltf"
+};
 
 interface Badge3DProps {
-    model: string
+    model: string;
 }
 
 // const margin = 0.5
 
-const Badge3D = ({model}: Badge3DProps) => {
+const Badge3D = ({ model }: Badge3DProps) => {
     // @ts-ignore
-    const modelPath = MODEL_MAP[model] as string
-    const {scene} = useGLTF(modelPath)
-    // const {width, height} = useThree((state) => state.viewport)
+    const modelPath = MODEL_MAP[model] as string;
+    const { scene } = useGLTF(modelPath);
+    const [shouldRender, setShouldRender] = useState(true);
+
+    useEffect(() => {
+        setShouldRender(false);
+        setTimeout(() => {
+            setShouldRender(true);
+        });
+    }, [model]);
 
     return (
         <group dispose={null}>
-            <Float rotationIntensity={5} floatIntensity={6} speed={1.5}>
-                <Center>
-                    <Resize scale={5}>
-                        <primitive object={scene}/>
-                    </Resize>
-                </Center>
-            </Float>
+            {shouldRender && (
+                <Float rotationIntensity={5} floatIntensity={6} speed={1.5}>
+                    <Center>
+                        <Resize scale={5}>
+                            <primitive object={scene} />
+                        </Resize>
+                    </Center>
+                </Float>
+            )}
         </group>
-    )
-}
+    );
+};
 
 Object.values(MODEL_MAP).forEach((path) => {
-    useGLTF.preload(path)
-})
+    useGLTF.preload(path);
+});
 
-export default Badge3D
+export default Badge3D;
