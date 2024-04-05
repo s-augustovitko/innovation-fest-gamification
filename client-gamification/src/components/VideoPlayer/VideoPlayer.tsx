011304f0f52ff26897c9b32348aa7f889a289c57
Hugo Bernardo <hugo.bernardo@pluto.tv>
2024-04-05 18:27:53 -0300

    Spongebob video (#20)

--- client-gamification/src/components/VideoPlayer/VideoPlayer.tsx
+++ client-gamification/src/components/VideoPlayer/VideoPlayer.tsx
@@ -4,7 +4,7 @@ const VideoPlayer = () => {
     return (
         <div className={styles.container}>
             <video controls>
-                <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4"/>
+                <source src="https://ww.gotoon.tv/video/SpongeBob-SquarePants-Free-Samples.mp4" type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
         </div>
